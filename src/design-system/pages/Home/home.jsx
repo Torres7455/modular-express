3b47fe23344f2e4761eb4840e2fb9a16a059bc62
Andreshur07 <adhurtado@unimayor.edu.com>
2025-05-30 23:40:09 -0500

    cambio tab

--- src/design-system/pages/Home/home.jsx
+++ src/design-system/pages/Home/home.jsx
@@ -1,9 +1,34 @@
 // importacion
-
+import { Tabs } from "antd";
 
 //constantes globales
 
 //declaracion de la funcion
-export default function Home () {
-    return <>Hola soy el home test</>
+export default function Home() {
+  const onChange = (key) => {
+    console.log(key);
+  };
+  const items = [
+    {
+      key: "1",
+      label: "Tab 1",
+      children: "Content of Tab Pane 1",
+    },
+    {
+      key: "2",
+      label: "Tab 2",
+      children: "Content of Tab Pane 2",
+    },
+    {
+      key: "3",
+      label: "Tab 3",
+      children: "Content of Tab Pane 3",
+    },
+  ];
+  return (
+    <>
+      <Tabs defaultActiveKey="1" items={items} onChange={onChange} />
+      Hola soy el home test
+    </>
+  );
 }
